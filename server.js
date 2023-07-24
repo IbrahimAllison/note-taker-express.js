67d9d8de6995c166cb23e39ef8957c46d7b12465
Ibrahim Allison <ibrola5@gmail.com>
2023-07-24 15:50:59 -0500

    Added server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+// The code imports the required modules.
+const express = require('express');
+const html_routes = require('./routes/htmlRoutes')
+const api_routes = require('./routes/apiRoutes')
+const PORT = process.env.PORT || 1001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(api_routes)
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
